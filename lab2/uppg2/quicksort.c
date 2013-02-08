3f85fd39284dac3e17005a54dff8d0aede169877
Karl Johan Andreasson <kalleand@kth.se>
2013-02-08 15:47:22 +0100

    Added outline of quicksort. No algorithm is implemented.

--- lab2/uppg2/quicksort.c
+++ lab2/uppg2/quicksort.c
@@ -13,8 +13,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main(int argc, char ** argv)
+#define MAXSIZE 1000000 /* Maximum size of array to be sorted. */
+#define BREAKPOINT 50 /* Breakpoint when insertion sort is made instead of quicksort. */
+void quicksort(int, int);
+
+int size;
+int * vector;
+
+int main(int argc, char ** argv)
 {
-    printf("Hej\n");
+    if(argc > 1)
+    {
+        size = atoi(argv[1]);
+        if(size > MAXSIZE)
+        {
+            size = MAXSIZE;
+        }
+    }
+    else
+    {
+        size = MAXSIZE;
+    }
+    vector = malloc(sizeof(int) * size);
+    #pragma omp parallel
+    {
+        #pragma omp task
+        {
+            quicksort(0, size);
+        }
+    }
+    #pragma omp taskwait
+    free(vector);
     return(EXIT_SUCCESS);
+}
+
+void quicksort(int start, int end)
+{
+    if(end - start < BREAKPOINT)
+    {
+        /* TODO Insertionsort. */
+    }
+    else
+    {
+        /* TODO Quicksort. */
+    }
 }
