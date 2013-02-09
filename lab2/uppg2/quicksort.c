97332d4a4d88c29228984aee658faaa7f9f595f8
Karl Johan Andreasson <kalleand@kth.se>
2013-02-09 22:16:44 +0100

    Implemented quicksort and insertionsort. Is correct for usage where BREAKPOINT < 2.

--- lab2/uppg2/quicksort.c
+++ lab2/uppg2/quicksort.c
@@ -12,6 +12,8 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
 #define MAXSIZE 1000000 /* Maximum size of array to be sorted. */
 #define BREAKPOINT 50 /* Breakpoint when insertion sort is made instead of quicksort. */
@@ -22,6 +24,9 @@ int * vector;
 
 int main(int argc, char ** argv)
 {
+    int index;
+    bool sorted;
+    double start_time, end_time;
     if(argc > 1)
     {
         size = atoi(argv[1]);
@@ -35,25 +40,102 @@ int main(int argc, char ** argv)
         size = MAXSIZE;
     }
     vector = malloc(sizeof(int) * size);
-    #pragma omp parallel
+
+    srand(time(NULL));
+    for(index = 0; index < MAXSIZE; index++)
     {
-        #pragma omp single
+        vector[index] = rand();
+#ifdef DEBUG
+        printf("%d: %d\n", index, vector[index]);
+#endif
+    }
+#ifdef DEBUG
+    printf("--------- Sorting! ---------\n");
+#endif
+    start_time = omp_get_wtime();
+#pragma omp parallel
+    {
+#pragma omp single nowait
         {
             quicksort(0, size);
         }
     }
+    end_time = omp_get_wtime();
+#ifdef DEBUG
+    printf("------ Done sorting! -------\n");
+#endif
+    sorted = true;
+    for(index = 0; index < MAXSIZE; index++)
+    {
+        if(index != 0)
+        {
+            if(vector[index] < vector[index - 1])
+                sorted = false;
+        }
+#ifdef DEBUG
+        printf("%d: %d\n", index, vector[index]);
+#endif
+    }
+
     free(vector);
+
+    if(sorted)
+        printf("The vector was sorted!\n");
+    else
+        printf("Sorting failed!\n");
+    printf("Program took %g seconds to sort.\n", end_time - start_time);
     return(EXIT_SUCCESS);
+}
+
+void swap_place(int a, int b)
+{
+    int tmp;
+    if(a == b) return;
+    tmp = vector[a];
+    vector[a] = vector[b];
+    vector[b] = tmp;
 }
 
 void quicksort(int start, int end)
 {
     if(end - start < BREAKPOINT)
-    {
-        /* TODO Insertionsort. */
+    { /* Insertionsort. */
+        int i;
+        for(i = start + 1; i < end; i++)
+        {
+            int value_to_insert = vector[i];
+            int hole_pos = i;
+            while(hole_pos > start && value_to_insert < vector[hole_pos - 1])
+            {
+                vector[hole_pos] = vector[hole_pos - 1];
+                hole_pos--;
+            }
+            vector[hole_pos] = value_to_insert;
+        }
     }
     else
-    {
-        /* TODO Quicksort. */
+    { /* Quicksort. */
+        int pivot = vector[end - 1]; /* NOT end because that points to one after last element. */
+        int store_index = start;
+        int i;
+        for(i = start; i < end - 1; i++)
+        {
+            if(vector[i] < pivot)
+            {
+                swap_place(i, store_index);
+                store_index++;
+            }
+        }
+
+        swap_place(end - 1, store_index);
+
+#pragma omp task
+        {
+            quicksort(start, store_index);
+        }
+#pragma omp task
+        {
+            quicksort(store_index + 1, end);
+        }
     }
 }
