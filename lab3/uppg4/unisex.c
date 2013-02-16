9d9d0e5d06e4e10ea2c6dcdb391838f79000fb44
Karl Johan Andreasson <kalleand@kth.se>
2013-02-16 16:09:57 +0100

    Commented the code of unisex.c.

--- lab3/uppg4/unisex.c
+++ lab3/uppg4/unisex.c
@@ -10,38 +10,48 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#define SHARED 0
-#define MAXMALES 10
-#define MAXWOMEN 10
-#define MAXTIMES 20
-#define MAXTIMEIN 3
-#define MINTIMEIN 1
-#define MAXINTERVAL 5
-#define MININTERVAL 1
+#define SHARED 0        /* The semaphores should not be shared. */
+#define MAXMALES 10     /* The max number of males possible. */
+#define MAXWOMEN 10     /* The max number of women possible. */
+#define MAXTIMES 20     /* Max number of times the bathroom is used. */
+#define MAXTIMEIN 3     /* Max seconds in the bathroom. */
+#define MINTIMEIN 1     /* Min seconds in the bathroom. */
+#define MAXINTERVAL 5   /* Max time between visits to the bathroom. */
+#define MININTERVAL 1   /* Min time between visits to the bathroom. */
 
-void * male(void *);
-void * female(void *);
+void * male(void *);    /* Male method declaration. */
+void * female(void *);  /* Female method declaration. */
 
-sem_t crit_sem, waiting_male_sem, waiting_female_sem;
-int males_inside, females_inside, males_waiting, females_waiting;
+sem_t crit_sem, waiting_male_sem, waiting_female_sem; /* Semaphores used. */
 
-int times_used;
+/* Counters used. */
+int males_inside, females_inside, males_waiting, females_waiting, times_used;
 
+/* Bool to keep record of we should let the other of the same gender in.
+ * (Only one thread should do this.) */
 bool letting_in_people;
 
+/* Number of males and females specified by the user. */
 int males, women;
 
+/**
+ * Main method, initiates all the semaphores and counters and then starts the
+ * specified number of males and females. Then joines them together again after
+ * the bathroom has been visited MAXTIMES number of times.
+ */
 int main(int argc, char ** argv)
 {
     pthread_attr_t attr;
-    pthread_t persons[MAXMALES + MAXWOMEN];
-    int index;
+    pthread_t persons[MAXMALES + MAXWOMEN]; /* Threads that we should start. */
+    int index; /* Used to iterate over the threads. */
 
-    if(argc < 3)
+    if(argc < 3) /* Check if insufficient number of arguments has been entered. */
     {
         printf("Usage ./unisex.out MALES WOMEN\n");
         return EXIT_SUCCESS;
     }
+    /* Read the number of males and females and make sure they are within the 
+     * allowed range. */ 
     else
     {
         males = atoi(argv[1]);
@@ -50,103 +60,159 @@ int main(int argc, char ** argv)
         if(women > MAXWOMEN) women = MAXWOMEN;
     }
 
-
-
+    /* Initialize the pthread attribute. */
     pthread_attr_init(&attr);
     pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
 
+    /* Initialize the semaphores used. */
     sem_init(&crit_sem, SHARED, 1);
     sem_init(&waiting_male_sem, SHARED, 0);
     sem_init(&waiting_female_sem, SHARED, 0);
 
+    /* Initialize the counters. */
     males_inside = 0;
     females_inside = 0;
     males_waiting = 0;
     females_waiting = 0;
 
-    letting_in_people = false;
+    letting_in_people = false; /* We are currently not letting people inside. */
 
-    srand(time(NULL));
+    srand(time(NULL)); /* Seed the randomizer to provide different results. */
 
-    for(index = 0; index < males; index++)
+    for(index = 0; index < males; index++) /* Creates the males. */
     {
         pthread_create(&persons[index], &attr, male, NULL);
     }
-    for(index = 0; index < women; index++)
+    for(index = 0; index < women; index++) /* Creates the females. */
     {
         pthread_create(&persons[males + index], &attr, female, NULL);
     }
-    for(index = 0; index < males + women; index++)
+    for(index = 0; index < males + women; index++) /* Joins the threads again. */
     {
         pthread_join(persons[index], NULL);
     }
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS; /* Once we joined the thread we exit. */
 }
 
+/**
+ * Prints the current status of the bathroom and its two queues.
+ */
 void print_status()
 {
     printf("\nINSIDE: M=%d, F=%d.\nWAITING: M=%d, F=%d.\n\n", males_inside, females_inside, males_waiting, females_waiting);
 }
 
+/**
+ * Male and female version is extremely similair (as expected).
+ *
+ * Male version of the simulation of the Unisex bathroom. This method will try to enter
+ * the bathroom until it has been used MAXTIMES number of times.
+ *
+ * If nobody is inside or only males are inside with no females waiting we enter.
+ * Else we have to wait for the next time it will be males turn to enter hence we wait for
+ * waiting_male_sem. Once we got this we can safely enter the bathroom. However we might want to
+ * let the other males waiting enter as well (if there are any and we are the first male to try
+ * to enter the bathroom. Then simulate the usage of the bathroom by sleeping a random amount
+ * of time. Then it is time to exit, when we exit and we are the last to exit we pass the baton
+ * to a female (if one is waiting). This is to control who gets to enter, and since it is the
+ * females turn we pass the baton to the head of the queue for waiting_female_sem.
+ */
 void * male(void * input)
 {
-    int i;
+    int i; /* Index used for alerting other males. */
+
+    /* Number of males in queue at the moment it is the males turn. */
     int males_in_queue;
+
+    /* Main loop. Only use the bathroom MAXTIMES number of times. */
     while(times_used < MAXTIMES)
     {
         /* We simulate time until we need to go to the bathroom. */
         sleep((rand() % (MAXINTERVAL - MININTERVAL)) + MININTERVAL);
 
-        sem_wait(&crit_sem);
-        males_waiting++;
+        sem_wait(&crit_sem); /* Critical section began. */
+        males_waiting++; /* Increase the number of males waiting to enter. */
+
+        /* Print status. */
         printf("Male trying to enter bathroom.\n");
         print_status();
 
+        /* If we are not allowed to enter. */
         if(females_inside > 0 || females_waiting > 0)
         {
+            /* Print status. */
             printf("Males waiting for their turn.\n");
             print_status();
+
+            /* Release mutual exclusion semaphore. */
             sem_post(&crit_sem);
             sem_wait(&waiting_male_sem); /* Waiting for our turn. */
-            males_inside++; /* We use pass the baton technique. */
+
+            /*
+             * NOTE: We now have the crit_sem because of passing the
+             * baton technique. 
+             */
+
+            /* Update status to represent us being inside. */
+            males_inside++;
             males_waiting--;
+
+            /* If we are responsible of letting rest of the males inside. */
             if(letting_in_people == false)
             {
-                letting_in_people = true;
+                letting_in_people = true; /* Only one should do this. */
+
+                /* Number of males we should let in. */
                 males_in_queue = males_waiting;
+
+                /* Print status. */
                 printf("Males turn, first one entering.\n");
                 print_status();
+
+                /* Let the other males in queue inside the bathroom.
+                 * We have to accuire the crit_sem every time because
+                 * of pass the baton technique. */
                 for(i = 0; i < males_in_queue; i++)
                 {
                     sem_post(&waiting_male_sem);
                     sem_wait(&crit_sem);
                 }
+                /* We are done letting males inside. */
                 letting_in_people = false;
             }
             else
             {
+                /* Print status of us entering. */
                 printf("Male entering.\n");
                 print_status();
             }
 
         }
-        else /* Females are already inside. */
+        else /* Males are already inside. */
         {
             males_inside++;
             males_waiting--;
+
+            /* Print status of us entering. */
             printf("Male entering.\n");
             print_status();
         }
-        sem_post(&crit_sem);
+        sem_post(&crit_sem); /* We are done with the critical section. */
 
         /* Simulate going to the toilet. */
         sleep((rand() % (MAXTIMEIN - MINTIMEIN)) + MINTIMEIN);
 
-        sem_wait(&crit_sem);
-        times_used++;
-        males_inside--;
+        /* Now we exit the bathroom. */
+        sem_wait(&crit_sem);    /* Begin critical section. */
+        times_used++;           /* Increase the counter of times used. */
+        males_inside--;         /* We are no longer inside the bathroom. */
+
+        /* Print status to reflect us exiting the toilet. */
         printf("Male exiting bathroom.\n");
         print_status();
+
+        /* If we are the last male to exit the bathroom we want to pass the baton
+         * to a female if there is one waiting to enter. */
         if(males_inside == 0)
         {
             printf("Last male has exited bathroom.\n");
@@ -156,47 +222,93 @@ void * male(void * input)
             else
                 sem_post(&crit_sem);
         }
-        else
+        else /* Else we exit critical section. */
             sem_post(&crit_sem);
     }
     pthread_exit(NULL);
 }
 
+/**
+ * Male and female version is extremely similair (as expected).
+ *
+ * Female version of the simulation of the Unisex bathroom. This method will try to enter
+ * the bathroom until it has been used MAXTIMES number of times. 
+ * 
+ * If nobody is inside or only females are inside with no males waiting we enter.
+ * Else we have to wait for the next time it will be females turn to enter hence we wait for
+ * waiting_female_sem. Once we got this we can safely enter the bathroom. However we might want to
+ * let the other females waiting enter as well (if there are any and we are the first female to try
+ * to enter the bathroom. Then simulate the usage of the bathroom by sleeping a random amount
+ * of time. Then it is time to exit, when we exit and if we are the last to exit we pass the baton
+ * to a male (if one is waiting). This is to control who gets to enter, and since it is the
+ * males turn we pass the baton to the head of the queue for waiting_male_sem.
+ */
 void * female(void * input)
 {
-    int i;
-    int females_in_queue;
+    int i; /* Index used to iterate over the females to let inside the bathroom. */
+    int females_in_queue; /* Number of females to let inside the bathroom. */
+
+    /* Main loop executed a total MAXTIMES number of times between males and females. */
     while(times_used < MAXTIMES)
     {
         /* We simulate time until we need to go to the bathroom. */
         sleep((rand() % (MAXINTERVAL - MININTERVAL)) + MININTERVAL);
 
+        /* Start of the critical section. */
         sem_wait(&crit_sem);
+
+        /* Increase the number of females waiting to enter the bathroom. */
         females_waiting++;
+
+        /* Print status to signal a new female has arrived. */
         printf("Female trying to enter bathroom.\n");
         print_status();
 
+        /* We check if we have to wait for it to be our turn. */
         if(males_inside > 0 || males_waiting > 0)
         {
+            /* Print status to show that we are waiting. */
             printf("Females waiting for their turn.\n");
             print_status();
-            sem_post(&crit_sem);
+
+            sem_post(&crit_sem); /* Exit critical section. */
             sem_wait(&waiting_female_sem); /* Waiting for our turn. */
-            females_inside++; /* We use pass the baton technique. */
+
+            /*
+             * NOTE: We now have the crit_sem because of passing the
+             * baton technique. 
+             */
+
+            /* Update the status. */
+            females_inside++;
             females_waiting--;
+
+            /* If we are the first in line of females to enter we let the
+             * rest of the females in the queue know that they can enter as
+             * well. */
             if(letting_in_people == false)
             {
-                letting_in_people = true;
+                letting_in_people = true; /* Only one female should do this. */
+
+                /* Number of females in the queue at this time. */
                 females_in_queue = females_waiting;
+
+                /* Print the status to show that females are now allowed to enter. */
                 printf("Females turn, first one entering.\n");
                 print_status();
+
+                /* Let the rest of the females in the queue enter.
+                 * We pass the baton to every female in queue hence we need to
+                 * reaccuire the crit_sem after each female is alerted. */
                 for(i = 0; i < females_in_queue; i++)
                 {
                     sem_post(&waiting_female_sem);
                     sem_wait(&crit_sem);
                 }
+                /* We are now done letting females inside. */
                 letting_in_people = false;
             }
+            /* We are free to enter. */
             else
             {
                 printf("Female entering.\n");
@@ -205,30 +317,42 @@ void * female(void * input)
         }
         else /* Females are already inside. */
         {
+            /* Update counters and print status to reflect this change. */
             females_inside++;
             females_waiting--;
             printf("Female entering.\n");
             print_status();
         }
-        sem_post(&crit_sem);
+        sem_post(&crit_sem); /* End of critical section. */
 
         /* Simulate going to the toilet. */
         sleep((rand() % (MAXTIMEIN - MINTIMEIN)) + MINTIMEIN);
 
-        sem_wait(&crit_sem);
-        times_used++;
-        females_inside--;
+        sem_wait(&crit_sem);    /* Start of critical section to exit bathroom. */
+        times_used++;           /* Increas the number of times 
+                                   the bathroom has been used. */
+        females_inside--;       /* We are no longer inside the bathroom. */
+
+        /* Prints the status to show that we exited. */
         printf("Female exiting bathroom.\n");
         print_status();
+
+        /* If we are the last female to exit the bathroom we let males enter if there
+         * are any in the queue. */
         if(females_inside == 0)
         {
+            /* Prints status to show we are the last female. */
             printf("Last female has exited bathroom.\n");
             print_status();
+
+            /* If there are any males waiting we pass the baton else just exit 
+             * critical seciton. */
             if(males_waiting > 0)
                 sem_post(&waiting_male_sem);
             else
                 sem_post(&crit_sem);
         }
+        /* If there are more females inside we can safely exit the critical section. */
         else
             sem_post(&crit_sem);
     }
